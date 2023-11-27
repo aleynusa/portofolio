97606731b03d47e94268871b43d45332ec1fd97f
Ariel Setiawan <alenusa04@gmail.com>
2023-11-28 04:41:39 +0800

    responsive 2

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,6 +1,8 @@
 "use client";
 import Link from "next/link";
 import { useState } from "react";
+import { Popover } from "@headlessui/react";
+import { Bars3Icon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,78 +13,25 @@ const Navbar = () => {
 
   return (
     <div className="flex flex-row justify-between items-center py-5 px-7 bg-gray-800 text-white rounded-xl md:w-full">
-      <div className="font-bold">PORTOFOLIO</div>
-      <div className="flex flex-row justify-between w-1/5 lg:visible invisible">
+      <div className="font-bold w-1/2">PORTOFOLIO</div>
+      <div className="flex flex-row justify-between w-1/2 lg:visible invisible">
         <Link href="#"> About </Link>
         <Link href="#"> Projects</Link>
         <Link href="#"> Contact</Link>
       </div>
-      <div>
-        <button
-          onClick={handleClick}
-          data-collapse-toggle="navbar-hamburger"
-          type="button"
-          class="inline-flex items-center justify-center p-2 w-10 h-10 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          aria-controls="navbar-hamburger"
-          aria-expanded="false"
-        >
-          <span class="sr-only">Open main menu</span>
-          <svg
-            class="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        <div
-          className={`${isOpen ? "" : "hidden"} w-full`}
-          id="navbar-hamburger"
-        >
-          <ul class="flex flex-col font-medium mt-4 rounded-lg bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-white bg-blue-700 rounded dark:bg-blue-600"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                Services
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                Pricing
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
+      <div className="w-1/2 flex justify-end md:pr-16 items-center md:visible invisible">
+        <Popover className="relative">
+          <Popover.Button>
+            <Bars3Icon className="h-6 w-6 text-white" />
+          </Popover.Button>
+          <Popover.Panel className="absolute z-10 bg-gray-600 rounded-xl flex p-7">
+            <div className="flex flex-col gap-7 py-5">
+              <a href="#">About</a>
+              <a href="#">Projects</a>
+              <a href="https://www.instagram.com/viezcenleyy/">Contact</a>
+            </div>
+          </Popover.Panel>
+        </Popover>
       </div>
     </div>
   );
